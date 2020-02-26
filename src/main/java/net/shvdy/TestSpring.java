56417f94ab8516054cd27999b769e25dc22a3506
shvdy <flylikearocket@gmail.com>
2020-02-26 10:48:38 +0200

    Lesson 5 (DI)

--- src/main/java/net/shvdy/TestSpring.java
+++ src/main/java/net/shvdy/TestSpring.java
@@ -16,9 +16,9 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        Music musicBean = context.getBean("musicBean", Music.class);
 
-        MusicalPlayer mp = new MusicalPlayer(musicBean);
+        Music musicBean = context.getBean("musicBean", Music.class);
+        MusicalPlayer mp = context.getBean("musicalPlayer", MusicalPlayer.class);
         System.out.println(mp.playMusic());
 
         context.close();
